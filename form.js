6edca106e36043a1ac0dc9ee29b170e6953c668c
Nick Vermonden <nvermonden@unitedspb.eu>
2022-10-26 11:23:34 +0200

    add errors messages

--- form.js
+++ form.js
@@ -2,6 +2,7 @@ let barcodeForm = document.getElementById('barcodeform')
 let submitButton = document.getElementById('submitbutton')
 let oldbarcodes = document.getElementById('oldbarcodes')
 let newbarcodeslist = document.getElementById('fixedlist')
+let errorlist = document.getElementById('errors')
 
 submitButton.addEventListener("click", function(e){
     e.preventDefault();
@@ -22,7 +23,20 @@ submitButton.addEventListener("click", function(e){
                 let li = document.createElement("li");
                 li.innerText = '00' + oldCode;
                 newbarcodeslist.appendChild(li);
-            } else {
+            } else if(oldCode.length < 11 && oldCode.length != 0 || oldCode.length > 13) {
+                let li = document.createElement("li");
+                li.innerHTML = '<span style="color:red;">Check this code please</span>';
+                newbarcodeslist.appendChild(li);
+
+                let div = document.createElement("div");
+                div.innerHTML = '<span style="color:red;">There are some errors, check list</span>';
+                errorlist.appendChild(div);
+
+            } else if(oldCode.length === ' '){
+                let li = document.createElement("li");
+                li.innerHTML = '<br />';
+                newbarcodeslist.appendChild(li);
+            }else {
                 let li = document.createElement("li");
                 li.innerHTML = '<br />';
                 newbarcodeslist.appendChild(li);
